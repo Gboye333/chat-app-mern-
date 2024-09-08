a5971f6de01911247ebacd604765ddd896c9cb5e
Joseph <gboyes333@gmail.com>
2024-09-09 00:09:39 +0100

    second part push

---
+++ frontend/src/components/sideBar/sideBar.jsx
@@ -0,0 +1,17 @@
+import SearchInput from "./searchInput"
+import Conversations from "./conversations"
+import LogoutButton from "./logoutButton"
+
+const sideBar = () => {
+  return (
+    <div className="border-r border-slate-500 p-4 flex flex-col">
+     <SearchInput/>
+     <div className="divider px-3"></div>
+       <Conversations/>
+      
+        <LogoutButton/> 
+    </div>
+  )
+}
+
+export default sideBar
